18c528379f8d5ba1fb981c2e74dad7bfdd559a9f
PerezFe <PerezFernandoE83@gmail.com>
2022-10-03 18:57:48 -0300

    titulo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div >
-      Prueba
+    <div className="text-center my-5">
+      <h1 >FORMULARIO</h1>
+      <hr/>
     </div>
   );
 }
